1706a484b2f7b5ac65154a16ee3416ffe71965c9
mkavcic <mkavcic1@etf.unsa.ba>
2018-10-27 17:28:46 +0200

    enum Grad, pokusaj #2

--- src/ba/unsa/etf/rpr/tutorijal03/FiksniBroj.java
+++ src/ba/unsa/etf/rpr/tutorijal03/FiksniBroj.java
@@ -1,12 +1,17 @@
 package ba.unsa.etf.rpr.tutorijal03;
 
 public class FiksniBroj implements TelefonskiBroj {
+    private Grad grad;
+    private String broj;
+
     public String ispisi(){
         String c="";
+
     return c;
     }
 
     public FiksniBroj(Grad grad, String broj) {
-
+        this.grad = grad;
+        this.broj = broj;
     }
 }
