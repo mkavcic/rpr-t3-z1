86ac2f0d72708103c37d1a9632f3c30eb845e9ed
mkavcic <mkavcic1@etf.unsa.ba>
2018-10-27 17:43:20 +0200

    Klasa Mobilni Broj, ispisi() nedovrsena

---
+++ src/ba/unsa/etf/rpr/tutorijal03/MobilniBroj.java
@@ -0,0 +1,16 @@
+package ba.unsa.etf.rpr.tutorijal03;
+
+public class MobilniBroj implements TelefonskiBroj{
+    private  int mobilnaMreza;
+    private String broj;
+
+    public MobilniBroj(int mobilnaMreza, String broj) {
+        this.mobilnaMreza = mobilnaMreza;
+        this.broj = broj;
+    }
+
+
+    public String ispisi() {
+    return "";
+    }
+}
