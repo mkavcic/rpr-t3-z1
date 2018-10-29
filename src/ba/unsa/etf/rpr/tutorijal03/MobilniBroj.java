630f8dc4790f81057c984e8fe18d16cd6a02c17c
mkavcic <mkavcic1@etf.unsa.ba>
2018-10-29 19:50:58 +0100

    MobilniBroj hashCode(), equals()

--- src/ba/unsa/etf/rpr/tutorijal03/MobilniBroj.java
+++ src/ba/unsa/etf/rpr/tutorijal03/MobilniBroj.java
@@ -14,4 +14,19 @@ public class MobilniBroj implements TelefonskiBroj{
         c += 0 + mobilnaMreza + "/" + broj;
         return c;
     }
+
+    @Override
+    public int hashCode(){
+        return mobilnaMreza;
+    }
+
+    @Override
+    public boolean equals(Object o){
+        if(o==null) return false;
+        else if(o instanceof MobilniBroj){
+            MobilniBroj a=(MobilniBroj) o;
+            if(a.broj.equals(this.broj) && a.hashCode() == this.hashCode()) return true;
+        }
+        return false;
+    }
 }
