62843517cfa292d8867b561ad9d6e2dab4437d28
mkavcic <mkavcic1@etf.unsa.ba>
2018-10-29 19:02:52 +0100

    Klasa Imenik, prepravke

--- src/ba/unsa/etf/rpr/tutorijal03/Imenik.java
+++ src/ba/unsa/etf/rpr/tutorijal03/Imenik.java
@@ -1,13 +1,28 @@
 package ba.unsa.etf.rpr.tutorijal03;
 
 import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Set;
+//import java.util.Map;
 
 public class Imenik {
-    private HashMap h;
-    void dodaj(String ime, TelefonskiBroj broj){
-        h.put(broj, ime);
+    private HashMap imenikPoImenu, imenikPoBroju;
+    public void dodaj(String ime, TelefonskiBroj broj){
+        imenikPoBroju.put(broj, ime);
+        imenikPoImenu.put(ime, broj);
     }
-    String dajBroj(String ime){
+
+    /*public String dajBroj(String ime){
+        Iterator i=h.entrySet().iterator();
+        while(i.hasNext()){
+           Set s= (Set) i.next();
+        }
     return "";
-    }
+    }*/
+
+   /* public Set<String> izGrada(Grad g){
+         Set s[];
+         if()
+
+    }*/
 }
